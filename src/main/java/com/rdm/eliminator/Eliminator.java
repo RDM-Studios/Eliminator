396414c03c8029686b39f66db7687a1317a25bdb
RaveTr <yuyuss210@gmail.com>
2023-04-30 20:18:00 +0300

    Initial Commioit (real)

---
+++ src/main/java/com/rdm/eliminator/Eliminator.java
@@ -0,0 +1,14 @@
+package com.rdm.eliminator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Eliminator implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("eliminator");
+
+    @Override
+    public void onInitialize() {
+    }
+}
